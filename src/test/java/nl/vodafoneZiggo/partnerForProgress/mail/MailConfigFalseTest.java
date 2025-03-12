c73bcbf99075b845ab4c8bb0e3ed1f3ea69e0090
FabianHartman <fabian.hartman@student.hu.nl>
2025-03-12 18:13:13 +0100

    added test to test the mailConfig

---
+++ src/test/java/nl/vodafoneZiggo/partnerForProgress/mail/MailConfigFalseTest.java
@@ -0,0 +1,27 @@
+package nl.vodafoneZiggo.partnerForProgress.mail;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+@SpringBootTest
+@TestPropertySource(properties = "mail.sendMails=false")
+class MailConfigFalseTest {
+    @Autowired
+    private MailConfig mailConfig;
+    @Autowired
+    private NylasMail nylas;
+    @Autowired
+    private PrintlnMail println;
+
+    @Test
+    @DisplayName("Should use PrintlnMail when mail.sendMails is false")
+    void shouldUseNylasMail() {
+        Mail mail = mailConfig.mailImplementation(nylas, println);
+        assertInstanceOf(PrintlnMail.class, mail, "Expected PrintlnMail as the implementation");
+    }
+}
