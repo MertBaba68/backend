e76a68e32b2f28b554c4a418ed47d927a8ff46ca
FabianHartman <fabian.hartman@student.hu.nl>
2025-03-19 11:15:25 +0100

    added role field to contact form

--- src/main/java/nl/vodafoneZiggo/partnerForProgress/services/application/dto/ContactDTO.java
+++ src/main/java/nl/vodafoneZiggo/partnerForProgress/services/application/dto/ContactDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 public class ContactDTO {
     private String companyName;
     @Email(message = "Invalid email format")
+    private String role;
     private String email;
     private String phone;
     private String contactPersonName;
@@ -14,8 +15,9 @@ public class ContactDTO {
     protected ContactDTO() {
     }
 
-    public ContactDTO(String companyName, String email, String phone, String contactPersonName, String location, String context) {
+    public ContactDTO(String companyName, String role, String email, String phone, String contactPersonName, String location, String context) {
         this.companyName = companyName;
+        this.role = role;
         this.email = email;
         this.phone = phone;
         this.contactPersonName = contactPersonName;
@@ -29,6 +31,14 @@ public class ContactDTO {
 
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
     public String getEmail() {
