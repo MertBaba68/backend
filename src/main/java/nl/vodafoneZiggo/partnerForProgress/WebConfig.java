75375468d0ace560e443a81bcb968f686cb0dc37
FabianHartman <fabian.hartman@student.hu.nl>
2025-03-19 13:12:09 +0100

    Added dev to corsconfig

--- src/main/java/nl/vodafoneZiggo/partnerForProgress/WebConfig.java
+++ src/main/java/nl/vodafoneZiggo/partnerForProgress/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","https://nice-forest-065ae2610-3.centralus.6.azurestaticapps.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .allowedHeaders("*");
