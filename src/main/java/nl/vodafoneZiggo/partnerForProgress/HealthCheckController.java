33b17184e1063343af2b0fa72a4ba72f46844cf3
MertBaba68 <mert.unlu@student.hu.nl>
2025-04-01 00:37:38 +0200

    healthcheck

---
+++ src/main/java/nl/vodafoneZiggo/partnerForProgress/HealthCheckController.java
@@ -0,0 +1,14 @@
+package nl.vodafoneZiggo.partnerForProgress;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+}
